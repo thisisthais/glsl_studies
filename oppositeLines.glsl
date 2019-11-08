f0f5203c89a8f21a72c4641f0ee9d33acdfc34f3
Thais Laney <laney.thais@gmail.com>
2019-11-08 15:01:46 -0500

    flicker lineup

--- oppositeLines.glsl
+++ oppositeLines.glsl
@@ -9,8 +9,8 @@ float random(in float x){
   return fract(sin(x)*1e4);
 }
 
-float randomLines(float x,float t){
-  return step(.5,random(floor(t)-fract(x)));
+float randomLines(float x,float freq,float t){
+  return step(.5,random(floor(t)-fract(x*freq)));
 }
 
 void main(void){
@@ -18,14 +18,23 @@ void main(void){
   st.x*=u_resolution.x/u_resolution.y;
   vec3 color=vec3(0.,0.,0.);
   
-  float stepCounter=floor(u_time/2.);
-  st*=vec2(1.,2.*min(stepCounter,100.));
-  float colTracker=step(1.,mod(st.y,2.));
+  // make two rows
+  st.y*=2.;
   
-  st.x+=colTracker*u_time/10000.;
-  st.x-=(1.-colTracker)*u_time/10000.;
+  float freq=random(floor(u_time/1000.))+abs(atan(u_time/1000.)*.1);;
+  float movement=u_time*(1.-freq);
   
-  color=vec3(randomLines(st.x,u_time));
+  // direction is 0 or 1
+  float direction=step(1.,mod(st.y,2.));
+  // direction is -1 or 1
+  direction=(direction-.5)*2.;
+  
+  float randCol=random(u_time*st.y);
+  color=vec3(
+    randomLines(st.x,freq,direction)+randCol,
+    randomLines(st.x,freq,direction)+randCol,
+    randomLines(st.x,freq,direction)+randCol
+  );
   
   gl_FragColor=vec4(color,1.);
 }
