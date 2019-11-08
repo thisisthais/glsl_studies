55d0f4c34e05b117a513820fbad330c14d6ee92b
Thais Laney <laney.thais@gmail.com>
2019-11-08 13:21:33 -0500

    pattern in noisy lines

--- oppositeLines.glsl
+++ oppositeLines.glsl
@@ -5,31 +5,27 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform float u_time;
 
-float random(in vec2 _st){
-  return fract(sin(dot(_st.xy,
-        vec2(12.9898,78.233)))*
-      43758.5453123);
-    }
-    
-    float random(in float x){
-      return fract(sin(x)*1e4);
-    }
-    
-    void main(void){
-      vec2 st=gl_FragCoord.xy/u_resolution.xy;
-      vec3 color=vec3(0.,0.,0.);
-      
-      float numLines=random(st.x);
-      st*=vec2(numLines,2.);
-      st=vec2(fract(st.x),st.y);
-      
-      float freq=u_time/100.;
-      float colTracker=step(1.,mod(st.y,2.));
-      
-      st.x-=2.*fract(u_time)*(1.-colTracker)+(1.-freq);
-      st.x+=2.*fract(u_time)*colTracker+freq;
-      
-      color=vec3(step(numLines,mod(st.x,2.)));
-      
-      gl_FragColor=vec4(color,1.);
-    }
+float random(in float x){
+  return fract(sin(x)*1e4);
+}
+
+float randomLines(float x,float t){
+  return step(.5,random(floor(t)-fract(x)));
+}
+
+void main(void){
+  vec2 st=gl_FragCoord.xy/u_resolution.xy;
+  st.x*=u_resolution.x/u_resolution.y;
+  vec3 color=vec3(0.,0.,0.);
+  
+  float stepCounter=floor(u_time/2.);
+  st*=vec2(1.,2.*min(stepCounter,100.));
+  float colTracker=step(1.,mod(st.y,2.));
+  
+  st.x+=colTracker*u_time/10000.;
+  st.x-=(1.-colTracker)*u_time/10000.;
+  
+  color=vec3(randomLines(st.x,u_time));
+  
+  gl_FragColor=vec4(color,1.);
+}
