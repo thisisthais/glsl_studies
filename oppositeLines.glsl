2e04d6947c86fb27fd6dc56f681b56408defdbd2
Thais Laney <laney.thais@gmail.com>
2019-11-08 15:08:10 -0500

    red cyan line glitch

--- oppositeLines.glsl
+++ oppositeLines.glsl
@@ -10,7 +10,7 @@ float random(in float x){
 }
 
 float randomLines(float x,float freq,float t){
-  return step(.5,random(floor(t)-fract(x*freq)));
+  return step(.2,random(floor(x*freq)-floor(t)));
 }
 
 void main(void){
@@ -18,23 +18,24 @@ void main(void){
   st.x*=u_resolution.x/u_resolution.y;
   vec3 color=vec3(0.,0.,0.);
   
+  float freq=random(floor(u_time))+random(floor(st.y));
+  float movement=u_time*(1.-freq)*20.;
+  
   // make two rows
   st.y*=2.;
-  
-  float freq=random(floor(u_time/1000.))+abs(atan(u_time/1000.)*.1);;
-  float movement=u_time*(1.-freq);
-  
   // direction is 0 or 1
   float direction=step(1.,mod(st.y,2.));
   // direction is -1 or 1
   direction=(direction-.5)*2.;
   
+  movement*=direction;
+  
   float randCol=random(u_time*st.y);
   color=vec3(
-    randomLines(st.x,freq,direction)+randCol,
-    randomLines(st.x,freq,direction)+randCol,
-    randomLines(st.x,freq,direction)+randCol
+    randomLines(st.x,freq*100.,movement-.25),
+    randomLines(st.x,freq*100.,movement),
+    randomLines(st.x,freq*100.,movement+.025)
   );
   
-  gl_FragColor=vec4(color,1.);
+  gl_FragColor=vec4(1.-color,1.);
 }
